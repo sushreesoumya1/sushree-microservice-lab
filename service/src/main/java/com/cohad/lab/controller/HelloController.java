d014d891b23dbea2ed1e3e583925206a1a0c6609
sushreesoumya1 <sushreesoumya.mishra@gmail.com>
2026-02-13 07:18:09 +0530

    v6: externalize config via ConfigMap

--- service/src/main/java/com/cohad/lab/controller/HelloController.java
+++ service/src/main/java/com/cohad/lab/controller/HelloController.java
@@ -2,6 +2,8 @@ package com.cohad.lab.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
+
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
@@ -9,21 +11,14 @@ import java.net.UnknownHostException;
 
 @RestController
 public class HelloController {
+    @Value("${app.message}")
+    private String message;
 
     @GetMapping("/hello")
     public String getMessage() throws UnknownHostException {
         String hostname = InetAddress.getLocalHost().getHostName();
-        int n=0;
-        for(int i=0;i<50;i++){
-            n+=i;
-        }
-        return n + "COHAD microservice lab - v3 🚀 | Pod: " + hostname;
+        return message + " | Pod: " + hostname;
     }
 
-    @GetMapping("/oom")
-    public String oom() {
-        byte[] memoryHog = new byte[700 * 1024 * 1024];
-        return "Allocated: " + memoryHog.length;
-    }
 
 }
